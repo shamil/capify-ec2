ab7eac6abf35c9a6ab73eeab8100e48bca6fe676
Alex Simenduev <shamil@boxee.tv>
2012-02-08 11:38:11 -0500

    add support for environments

--- lib/capify-ec2.rb
+++ lib/capify-ec2.rb
@@ -27,8 +27,12 @@ class CapifyEc2
         :region => region
       )
 
+      project_env = fetch(:rails_env, nil)
       project_tag = ec2_config[:project_tag]
-      running_instances = ec2.servers.select {|instance| instance.state == "running" && (project_tag.nil? || instance.tags["Project"] == project_tag) }
+      running_instances = ec2.servers.select do |instance|
+        instance.state == "running" && instance.tags["rails_env"] == project_env &&
+          (project_tag.nil? || instance.tags["Project"] == project_tag)
+      end
 
       running_instances.each do |instance|
         instance.instance_eval do
